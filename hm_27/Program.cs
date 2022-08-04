cf2f247060ed40c6c1e66523f9d7921d6dfbc9fd
sysoev-kirill <sysoev.kirill@gmail.com>
2022-08-04 18:31:22 +0300

    task_27

---
+++ hm_27/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задача 27: Напишите программу, которая принимает на вход число и выдаёт
+//  сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int PlusNumber(int number)
+{
+   int sum = 0;
+
+   for (int i = 0; i < number; number /= 10)
+   {
+      sum = sum + number % 10;
+   }
+   return (sum);
+}
+int result = PlusNumber(num);
+
+Console.Write($"{num} => {result} ");
